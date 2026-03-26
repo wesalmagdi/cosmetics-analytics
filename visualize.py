8c2dcf25c64f444b820de07b72a85e40032fc850
Raneem Khaled <raneemkh2006@gmail.com>
2026-03-26 15:38:34 +0200

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -7,18 +7,17 @@ import subprocess
 file_path = sys.argv[1]
 df = pd.read_csv(file_path)
 
-num_cols = df.select_dtypes('number').columns
-
 fig, ax = plt.subplots(1, 3, figsize=(18, 5))
 
-sns.boxplot(y=df[num_cols[0]], ax=ax[0])
-ax[0].set_title('Boxplot')
+sns.countplot(x='brand', data=df, ax=ax[0])
+ax[0].set_title('Brand Count')
 
-sns.histplot(df[num_cols[0]], bins=20, ax=ax[1])
-ax[1].set_title('Histogram')
+df['country'].value_counts().plot.pie(ax=ax[1])
+ax[1].set_title('Country Proportion')
 
-sns.heatmap(df[num_cols].corr(), annot=True, cmap='coolwarm', ax=ax[2])
-ax[2].set_title('Correlation Heatmap')
+cross_tab = pd.crosstab(df['brand'], df['country'])
+sns.heatmap(cross_tab, annot=True, ax=ax[2])
+ax[2].set_title('Brand vs Country Heatmap')
 
 plt.tight_layout()
 plt.savefig("summary_plot.png")
